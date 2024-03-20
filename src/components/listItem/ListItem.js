c4cf7d139d94edb3517fd23e77ec7369ecefb93e
unknown <aaron.hickman201@gmail.com>
2024-03-20 19:05:34 +0000

    colour by type

--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./listItem.css";
 
 export default function ListItem({ pokemon }) {
+  // format gameindex to pokemon number system
   const formatGameindex = (num) => {
     if (num < 10) {
       return `00${num}`;
@@ -12,9 +13,80 @@ export default function ListItem({ pokemon }) {
     }
   };
 
+  const checkColourType = (pokeType) => {
+    for (let i = 0; i < backgrounColours.length; i++) {
+      if (pokeType === backgrounColours[i].type) {
+        return backgrounColours[i].colour;
+      }
+    }
+    return "#fbf7f5";
+  };
+
+  const backgrounColours = [
+    {
+      type: "fire",
+      colour: "#ff7a49",
+    },
+    {
+      type: "grass",
+      colour: "#FFB149",
+    },
+    {
+      type: "electric",
+      colour: "#FFFF32",
+    },
+    {
+      type: "bug",
+      colour: "#DCE319",
+    },
+    {
+      type: "flying",
+      colour: "#C8BFD4",
+    },
+    {
+      type: "ground",
+      colour: "#F2BD1B",
+    },
+    {
+      type: "steel",
+      colour: "#CBD1D4",
+    },
+    {
+      type: "poison",
+      colour: "#DDA0DD",
+    },
+    {
+      type: "water",
+      colour: "#9FE3EE",
+    },
+    {
+      type: "psychic",
+      colour: "#9E4773",
+    },
+    {
+      type: "dragon",
+      colour: "#D7CBBD",
+    },
+    {
+      type: "ghost",
+      colour: "#7E79A8",
+    },
+    {
+      type: "fighting",
+      colour: "#78A191",
+    },
+    {
+      type: "dark",
+      colour: "#C15EE9",
+    },
+  ];
+
   return (
-    <main className="main-item-container">
-      <div className="list-container">
+    <main
+      className="main-item-container"
+      style={{ backgroundColor: checkColourType(pokemon.types[0].type.name) }}
+    >
+      <div className="list-item-container">
         <div>{formatGameindex(pokemon.game_indices[19].game_index)}</div>
         <div className="list-name-container">{pokemon.name}</div>
       </div>
